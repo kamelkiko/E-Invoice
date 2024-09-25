1747ded9f0c3dd3b4b15d9f0fca8140e0c891e4c
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2024-09-25 13:16:24 +0300

    finish store freya decimal points

--- build.gradle.kts
+++ build.gradle.kts
@@ -83,11 +83,11 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Exe)
             packageName = "E-Receipt"
-            packageVersion = "1.0.0"
+            packageVersion = "1.0.1"
             windows {
-                packageVersion = "1.0.0"
-                msiPackageVersion = "1.0.0"
-                exePackageVersion = "1.0.0"
+                packageVersion = "1.0.1"
+                msiPackageVersion = "1.0.1"
+                exePackageVersion = "1.0.1"
                 includeAllModules = true
                 vendor = "AbApps"
                 description = "E-Receipt App"
